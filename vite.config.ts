55f1b362e1edd56439d1a6ce131950b65f48ef66
Eduardo Reusche Sáenz <ereusche@nequi.com>
2025-12-30 17:52:48 -0500

    v2.0

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/',
       server: {
-        port: 3000,
+        port: 3001,
         host: '0.0.0.0',
+        cors: true
+      },
+      build: {
+        assetsDir: 'assets'
       },
       plugins: [react()],
       define: {
